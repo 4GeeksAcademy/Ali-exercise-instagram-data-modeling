da0791c2eafee6d55f488a5312e109837ae0f978
AliRahimi123456 <162766248+AliRahimi123456@users.noreply.github.com>
2024-06-04 01:11:22 +0000

    Instagram-Data-Modeling is done

--- src/models.py
+++ src/models.py
@@ -7,26 +7,101 @@ from eralchemy2 import render_er
 
 Base = declarative_base()
 
-class Person(Base):
-    __tablename__ = 'person'
-    # Here we define columns for the table person
-    # Notice that each column is also a normal Python instance attribute.
-    id = Column(Integer, primary_key=True)
-    name = Column(String(250), nullable=False)
+class User(Base):
+    __tablename__ = 'user'
+ 
+    id = Column (Integer, primary_key=True)
+    username = Column(String(250), nullable=False)
+    first_name = Column(String(250), nullable=False)
+    last__name = Column(String(250), nullable=False)
+    password = Column(String(259), nullable=False)
+    phone = Column(String(250))
+    gender = Column(String(250))
 
-class Address(Base):
-    __tablename__ = 'address'
+    def __repr__(self):
+        return '<User %r>' % self.username
+    
+
+
+
+class Post(Base):
+    __tablename__ = 'post'
+    id = Column(Integer, primary_key=True)
+    title = Column(String(250), nullable=False)
+    description = Column(String(250), nullable=False)
+    icons = Column(String(250), nullable=False)
+    comments = Column(String(250))
+    user_id = Column(Integer, ForeignKey('user.id'))
+    user = relationship(User)
+
+    def __repr__(self):
+        return '<Post %r>' % self.title
+
+
+
+
+
+
+class Comment(Base):
+    __tablename__ = 'comments'
+    id = Column(Integer, primary_key=True)
+    content = Column(String(250), nullable=False)
+    user_id = Column(Integer, ForeignKey('user.id'))
+    user = relationship(User)
+    post_id = Column(Integer, ForeignKey('post.id'))
+    post = relationship(Post)
+
+    def __repr__(self):
+        return '<Post %r>' % self.content
+
+
+
+class Reaction(Base):
+    __tablename__ = 'reaction'
+    id = Column(Integer, primary_key=True)
+    number_of_likes = Column(Integer, nullable=False)
+    number_of_dislikes = Column(Integer, nullable=False)
+    post_id = Column(Integer, ForeignKey('posts.id'))
+    post = relationship(Post)
+    comment_id = Column(Integer, ForeignKey('comments.id'))
+    comment = relationship(Comment)
+
+    def __repr__(self):
+        return '<Post %r>' % self.id
+
+
+
+
+   
+class Follower(Base):
+    __tablename__ = 'followers'
+    id = Column(Integer, primary_key=True)
+    follower_user_id = Column(Integer, ForeignKey('user.id'))
+    followed_user_id = Column(Integer, ForeignKey('user.id'))
+    follower = relationship(User)
+    followed = relationship(User)
+
+    def __repr__(self):
+        return '<Post %r>' % self.id
+
+
+
+
+
+#class Address(Base):
+   # __tablename__ = 'address'
     # Here we define columns for the table address.
     # Notice that each column is also a normal Python instance attribute.
-    id = Column(Integer, primary_key=True)
-    street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
+   # id = Column(Integer, primary_key=True)
+    #street_name = Column(String(250))
+   # street_number = Column(String(250))
+   # post_code = Column(String(250), nullable=False)
+    # the two lins below establish the relationshipt between address table and person tables
+   # person_id = Column(Integer, ForeignKey('person.id'))
+   # person = relationship(Person)
 
-    def to_dict(self):
-        return {}
+    #def to_dict(self):
+       # return {}
 
 ## Draw from SQLAlchemy base
 try:
